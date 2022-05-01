bbdf104866937dc41a1244f15bc6a15987ec873e
jcybul <jcybul@wpi.edu>
2022-05-01 15:23:05 -0400

    update to predict on all including the ones labeled

--- train_weights.py
+++ train_weights.py
@@ -4,16 +4,17 @@ from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('joint.csv')
 cols = df[["Code",'Housing','Population','Income','Stars']]
+all = df[["Code",'Housing','Population','Income']]
 drop = cols.dropna(axis=0)
-predict = cols[cols['Stars'].isnull()]
-Y_p = predict[['Housing','Population','Income']].to_numpy()
-
+predict = cols[["Code",'Stars']]
+#Y_p = predict[['Housing','Population','Income']].to_numpy()
+Y_p_all = all[['Housing','Population','Income']].to_numpy()
 labels = drop["Stars"].to_numpy()
 X = drop[['Housing', 'Population','Income']].to_numpy()
 
 model = LinearRegression()
 model.fit(X, labels)
-s = model.predict(Y_p)
+s = model.predict(Y_p_all)
 pred = []
 for i in range(len(s)):
     if s[i] < 0:
@@ -27,6 +28,7 @@ final = predict[["Code","Stars"]]
 final.columns = ["Code","Predicted_Stars"]
 
 m = df.merge(final, left_on='Code', right_on='Code',how="outer")
+
 m.to_csv("joint.csv",index=False)
 
 
